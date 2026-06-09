b84ae2efe08638147a6e7c1542dc3cead27dc669
doomsday <intniogi@hotmail.com>
2026-06-09 11:13:07 +0300

    gameBoard created, function player created, 2 player objects created

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameBoard = {
+    board: [a, b, c, 
+            d, e, f, 
+            g, h, i]
+};
+
+function Player(name, mark) {
+    this.name = name;
+    this.mark = mark;
+}
+
+const playerOne = new Player(John, X)
+const playerTwo = new Player(Doe, O)
+
