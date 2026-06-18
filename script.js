7655bd79cfe995a712330fc2f8e37e6f8b394f19
doomsday <intniogi@hotmail.com>
2026-06-18 10:35:07 +0300

    created the UI, the game takes input for player name, the game announces winner, the game starts with new 'start' button, game refreshes/resets with new reset button

--- script.js
+++ script.js
@@ -1,156 +1,249 @@
-const Players = {
-    playerOne: {
-        name: 'Player 1',
-        mark: 'X'
-    },
-    playerTwo: {
-        name: 'Player 2',
-        mark: 'O'
+function Player(name, mark) {
+    this.name = name;
+    this.mark = mark;
+}
+
+const scoreDisplay = document.createElement('div')
+scoreDisplay.classList.add('score')
+scoreDisplay.textContent = ''
+document.body.appendChild(scoreDisplay)
+
+function check(arr, counter) {
+
+    let playerOneWon = false
+    let playerTwoWon = false
+    let draw = false
+
+    if (arr[0].textContent == arr[1].textContent && arr[0].textContent == arr[2].textContent && arr[0].textContent != '' && arr[1].textContent != '' && arr[2].textContent != '') {
+
+        let mark = arr[0].textContent
+        if (mark == 'X') {
+
+            console.log(gameController.playerOne.name)
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else if (mark == 'O') {
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+    }
+    else if (arr[3].textContent == arr[4].textContent && arr[4].textContent == arr[5].textContent && arr[3].textContent != '' && arr[4].textContent != '' && arr[5].textContent != '') {
+
+        let mark = arr[3].textContent
+
+        if (mark == 'X') {
+
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else {
+
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+    }
+    else if (arr[6].textContent == arr[7].textContent && arr[7].textContent == arr[8].textContent && arr[6].textContent != '' && arr[7].textContent != '' && arr[8].textContent != '') {
+
+        let mark = arr[6].textContent
+
+        if (mark == 'X') {
+
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else {
+
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+    }
+    else if (arr[0].textContent == arr[3].textContent && arr[3].textContent == arr[6].textContent && arr[0].textContent != '' && arr[3].textContent != '' && arr[6].textContent != '') {
+
+        let mark = arr[0].textContent
+        if (mark == 'X') {
+
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else {
+
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+    }
+    else if (arr[1].textContent == arr[4].textContent && arr[4].textContent == arr[7].textContent && arr[1].textContent != '' && arr[4].textContent != '' && arr[7].textContent != '') {
+
+        let mark = arr[4].textContent
+        if (mark == 'X') {
+
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else {
+
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+    }
+    else if (arr[2].textContent == arr[5].textContent && arr[5].textContent == arr[8].textContent && arr[2].textContent != '' && arr[5].textContent != '' && arr[8].textContent != '') {
+
+        let mark = arr[2].textContent
+        if (mark == 'X') {
+
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else {
+
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+
+    }
+    else if (arr[0].textContent == arr[4].textContent && arr[4].textContent == arr[8].textContent && arr[0].textContent != '' && arr[4].textContent != '' && arr[8].textContent != '') {
+
+        let mark = arr[0].textContent
+
+        if (mark == 'X') {
+
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else {
+
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+    }
+    else if (arr[2].textContent == arr[4].textContent && arr[4].textContent == arr[6].textContent && arr[2].textContent != '' && arr[4].textContent != '' && arr[6].textContent != '') {
+
+        let mark = arr[2].textContent
+
+        if (mark == 'X') {
+
+            scoreDisplay.textContent = gameController.playerOne.name + ' has won'
+            return playerOneWon = true
+        }
+        else {
+
+            scoreDisplay.textContent = gameController.playerTwo.name + ' has won'
+            return playerTwoWon = true
+        }
+    }
+    else if (counter == 9 && playerOneWon == false && playerTwoWon == false) {
+
+        scoreDisplay.textContent = ' Draw, reset the game and try again'
     }
 }
-console.log(Players)
 
-const gameBoard = {
-    board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    
-    gameplay: function play(player, boardSquare) {
-        let i = boardSquare
-        
-
-        if (player == 'Player 1') {
-            this.board[i] = 'X'
-            console.log(gameBoard.board)
-        } else {
-            this.board[i] = 'O'
-            console.log(gameBoard.board)
-        }
-    },
-    WinnerPlayerOne: function check(){
-        
-        if(this.board[0] == 'X' && this.board[1] == 'X' && this.board[2] == 'X'){
-            console.log('John wins')
-        }
-        else if(this.board[3] == 'X' && this.board[4] == 'X' && this.board[5] == 'X'){
-            console.log('John wins')
-        }
-        else if(this.board[6] == 'X' && this.board[7] == 'X' && this.board[8] == 'X'){
-            console.log('John wins')
-        }
-        else if(this.board[0] == 'X' && this.board[3] == 'X' && this.board[6] == 'X'){
-            console.log('John wins')
-        }
-        else if(this.board[1] == 'X' && this.board[4] == 'X' && this.board[7] == 'X'){
-            console.log('John wins')
-        }
-        else if(this.board[2] == 'X' && this.board[5] == 'X' && this.board[8] == 'X'){
-            console.log('John wins')
-        }
-        else if(this.board[0] == 'X' && this.board[4] == 'X' && this.board[8] == 'X'){
-            console.log('Player 1 wins')
-        }
-        else if(this.board[2] == 'X' && this.board[4] == 'X' && this.board[6] == 'X'){
-            console.log('John wins')
-        }
-        else {
-            return false
-        }
-        
-    },
-
-    WinnerPlayerTwo: function check() {
-        if(this.board[0] == 'O' && this.board[1] == 'O' && this.board[2] == 'O'){
-            console.log('Doe wins')
-        }
-        else if(this.board[3] == 'O' && this.board[4] == 'O' && this.board[5] == 'O'){
-            console.log('Doe wins')
-        }
-        else if(this.board[6] == 'O' && this.board[7] == 'O' && this.board[8] == '0'){
-            console.log('Doe wins')
-        }
-        else if(this.board[0] == 'O' && this.board[3] == 'O' && this.board[6] == 'O'){
-            console.log('Doe wins')
-        }
-        else if(this.board[1] == 'O' && this.board[4] == 'O' && this.board[7] == 'O'){
-            console.log('Doe wins')
-        }
-        else if(this.board[2] == 'O' && this.board[5] == 'O' && this.board[8] == 'O'){
-            console.log('Doe wins')
-        }
-        else if(this.board[0] == 'O' && this.board[4] == 'O' && this.board[8] == 'O'){
-            console.log('Doe wins')
-        }
-        else if(this.board[2] == 'O' && this.board[4] == 'O' && this.board[6] == 'O'){
-            console.log('Doe wins')
-        }
-        else {
-            return false
-        }
-    },
-    drawGame: function check() {
-        let playerOneCondition = gameBoard.WinnerPlayerOne()
-        let playerTwoCondition = gameBoard.WinnerPlayerTwo()
-
-        if (playerOneCondition === false & playerTwoCondition === false){
-            console.log('Draw')
-        }
-    }};
-
-const checkResult = () => {
-    return gameBoard.WinnerPlayerOne(), gameBoard.WinnerPlayerTwo(), gameBoard.drawGame()
-}
 
 const gameController = {
-    board: function generateBoard () {
+
+    playerOne: new Player('', 'X'),
+
+    playerTwo: new Player('', '0'),
+
+
+    board: function generateBoard() {
         const gameBox = document.createElement('div')
         document.body.appendChild(gameBox)
         const board = document.createElement('div')
         board.classList.add('board')
         gameBox.appendChild(board)
-        function createBoardCell () {
-            for (let i=1; i<= 9; i++) {
+
+        function createBoardCell() {
+
+            for (let i = 0; i <= 8; i++) {
                 const square = document.createElement('div')
                 board.appendChild(square)
+                square.classList.add('square')
+                square.style.width = `${680 / 3}px`
+                square.style.height = `${680 / 3}px`
+                square.style.border = '1px solid black'
+                square.style.fontSize = '150px'
             }
+
         }
         createBoardCell()
     },
+
+    boardCell: function groupSquares(arg) {
+
+        let playerOnePlayed = 0;
+        let playerTwoPlayed = 0;
+        let drawCounter = 0;
+
+        const allSquares = document.querySelectorAll('.square')
+
+        if (arg == 'start') {
+            allSquares.forEach((square) => {
+            square.addEventListener('click', () => {
+                let boardToArray = Array.from(allSquares)
+                
+                    if (square.textContent === '') {
+                        if (playerOnePlayed == playerTwoPlayed) {
+                            square.textContent = 'X'
+                            drawCounter++
+                            playerOnePlayed++
+                            check(boardToArray, drawCounter)
+
+
+                        } else if (playerOnePlayed > playerTwoPlayed && square.textContent != 'X' || square.textContent != 'O') {
+                            square.textContent = 'O'
+                            drawCounter++
+                            playerTwoPlayed++
+                            check(boardToArray, drawCounter)
+                        }
+                    }
+                    else {
+                        console.log('Invalid Action')
+                    }
+                })
+                
+            })}
+            else if (arg == 'reset') {
+                console.log('check')
+            allSquares.forEach((square) => {
+                
+                window.location.reload()
+
+            })    
+            }
+        },
+
+    form: function startGame(boardCellArg) {
+
+        let gameInitialization = gameController.board
+        gameInitialization()
+
+        let playerOneInput = document.getElementById('playerNameOne')
+        let playerTwoInput = document.getElementById('playerNameTwo')
+        scoreDisplay.innerHTML = playerOneInput.value + ' VS ' + playerTwoInput.value
+        return gameController.playerOne.name = playerOneInput.value, gameController.playerTwo.name = playerTwoInput.value
+    },
+
 }
 
+const startBtn = document.createElement('button')
+startBtn.classList.add('start')
+startBtn.textContent = 'Start'
+startBtn.addEventListener('click', () => {
+    let startForm = gameController.form
+    let startGame = gameController.boardCell
+    startForm()
+    startGame('start')
+})
 
-let gameInitialization = gameController.board
-gameInitialization()
+document.body.appendChild(startBtn)
 
-
-
-
-
-
-
-
-
-
-
-// // WINNER PLAYER ONE SCENARIO
-// gameBoard.gameplay('Player 1', 0)
-// gameBoard.gameplay('Player 2', 3)
-// gameBoard.gameplay('Player 1', 4)
-// gameBoard.gameplay('Player 2', 5)
-// gameBoard.gameplay('Player 1', 8)
-
-//DRAW SCENARIO
-
-// gameBoard.gameplay('John', 0)
-// gameBoard.gameplay('Doe', 1)
-// gameBoard.gameplay('John', 2)
-// gameBoard.gameplay('Doe', 3)
-// gameBoard.gameplay('John', 5)
-// gameBoard.gameplay('Doe', 4)
-// gameBoard.gameplay('John', 6)
-// gameBoard.gameplay('Doe', 8)
-// gameBoard.gameplay('John', 7)
-checkResult()
-
-
+const reset = document.createElement('button')
+reset.textContent = 'Reset'
+reset.addEventListener('click', () => {
+    let resetGame = gameController.boardCell
+    resetGame('reset')
+})
+document.body.appendChild(reset)
 
 
 
